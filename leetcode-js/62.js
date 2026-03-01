b81a6d751917fc48facdff8db39ed080999457f1
psan3333 <psan3333@mail.ru>
2026-03-01 12:17:11 +0700

    62: Unique Paths - Optimized memory usage

--- leetcode-js/62.js
+++ leetcode-js/62.js
@@ -4,21 +4,12 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
-    let grid = new Array(m);
-    for (let i = 0; i < m; i++) {
-        grid[i] = new Array(n).fill(0);
-    }
-    for (let i = 0; i < m; i++) {
-        grid[i][0] = 1;
-    }
-    for (let i = 0; i < n; i++) {
-        grid[0][i] = 1;
-    }
+    let grid = new Array(n).fill(1);
 
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
-            grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            grid[j] = grid[j - 1] + grid[j];
         }
     }
-    return grid[m - 1][n - 1];
+    return grid[n - 1];
 };
