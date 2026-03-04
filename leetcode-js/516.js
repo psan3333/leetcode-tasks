7c93c8d75ced62487e76934d0d38ae375278cffb
psan3333 <psan3333@mail.ru>
2026-03-04 10:15:14 +0700

    516: Longest Palindromic String - Solved

---
+++ leetcode-js/516.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function (s) {
+    let table = [];
+    for (let i = 0; i < s.length; i++) {
+        const arr = new Array(s.length).fill(0);
+        arr[i] = 1;
+        table.push(arr);
+    }
+
+    for (let j = 1; j < s.length; j++) {
+        for (let i = j - 1; i >= 0; i--) {
+            if (s[i] === s[j]) {
+                table[i][j] = table[i + 1][j - 1] + 2;
+            } else {
+                table[i][j] = Math.max(table[i + 1][j], table[i][j - 1]);
+            }
+        }
+    }
+    return table[0][s.length - 1];
+};
+
+const s = "cbbddd";
+console.log(longestPalindromeSubseq(s));
